5e51eff6aadcfc49e1574ad78bef5bbb9c75e2c4
stevenJSP <stevenJSP@gitee.com>
2025-06-18 10:24:26 +0800

    clock第一次

---
+++ src/Clock.java
@@ -0,0 +1,18 @@
+
+public class Clock {
+    private Display hours = new Display(24);
+    private Display minutes = new Display(60);
+
+    public void start(){
+        while (true) {
+            minutes.increase();
+            if (minutes.getValue() == 0)
+                hours.increase();
+            System.out.printf("%02d:%02d\n", hours.getValue(), minutes.getValue());
+        }
+    }
+    public static void main(String[] args) {
+        Clock clock = new Clock();
+        clock.start();
+    }
+}
